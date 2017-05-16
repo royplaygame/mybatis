0a5e46c7d92fd7e0e2f84e715c2573ad161eac2a
root <root@test.com>
2017-05-16 22:35:21 +0800

    my first test

---
+++ src/com/hy/ly/entity/UserInfo.java
@@ -0,0 +1,52 @@
+package com.hy.ly.entity;
+
+import java.util.Date;
+
+public class UserInfo {
+	private int userid;
+	private String username;
+	private Date birthday;
+	private String sex;
+	private String address;
+
+	public int getUserid() {
+		return userid;
+	}
+
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
---
+++ src/com/hy/ly/mybatis/first/MybatisFirst.java
@@ -0,0 +1,39 @@
+package com.hy.ly.mybatis.first;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import com.hy.ly.entity.UserInfo;
+
+public class MybatisFirst {
+
+	// 根据userid查询用户信息
+	@Test
+	public void findUserInfoById() throws IOException {
+		SqlSession sqlSession = null;
+		try {
+			// mybatis配置文件
+			String resource = "SqlMapConfig.xml";
+			// 获取流
+			InputStream is = Resources.getResourceAsStream(resource);
+			// 创建会话工厂
+			SqlSessionFactory sf = new SqlSessionFactoryBuilder().build(is);
+			// 创建sqlSession
+			sqlSession = sf.openSession();
+			// 通过sqlSession操作数据库
+			UserInfo userInfo = sqlSession.selectOne("test.findUserInfoById", 1001);
+			System.out.println(userInfo);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			sqlSession.close();
+		}
+
+	}
+}
