0c4a93034023fa3850001c92dd2fd815e14ed130
root <root@test.com>
2017-05-18 12:41:43 +0800

    update

--- test/com/hy/ly/dao/UserInfoDaoImplTest.java
+++ test/com/hy/ly/dao/UserInfoDaoImplTest.java
@@ -16,34 +16,37 @@ public class UserInfoDaoImplTest {
 	private SqlSessionFactory sqlSessionFactory;
 
 	@Before
-	public void setUp() throws Exception{
-		// mybatis閰嶇疆鏂囦欢
+	public void setUp() throws Exception {
+		// mybatis配置文件
 		String resource = "SqlMapConfig.xml";
-		// 鑾峰彇娴�
+		// 获取流
 		InputStream is = Resources.getResourceAsStream(resource);
-		// 鍒涘缓浼氳瘽宸ュ巶
+		// 创建会话工厂
 		sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
 	}
 
 	@Test
 	public void findTest() throws Exception {
 		UserInfoDao userInfoDao = new UserInfoDaoImpl(sqlSessionFactory);
-		UserInfo userInfo=userInfoDao.findUserById(1105);
+		UserInfo userInfo = userInfoDao.findUserById(1105);
 		System.out.println(userInfo);
 	}
+
 	@Test
 	public void addTest() throws Exception {
 		UserInfoDao userInfoDao = new UserInfoDaoImpl(sqlSessionFactory);
-		UserInfo userInfo=new UserInfo(1000,"sunshangxiang",new Date(),"女","河南开封");
+		UserInfo userInfo = new UserInfo(1000, "sunshangxiang", new Date(), "女", "河南开封");
 		userInfoDao.addUserInfo(userInfo);
 		System.out.println(userInfo.getUserid());
 	}
+
 	@Test
 	public void updateTest() throws Exception {
 		UserInfoDao userInfoDao = new UserInfoDaoImpl(sqlSessionFactory);
-		UserInfo userInfo=new UserInfo(1002,"孙尚香",new Date(),"女","河南开封");
+		UserInfo userInfo = new UserInfo(1002, "孙尚香", new Date(), "女", "河南开封");
 		userInfoDao.updateUserInfo(userInfo);
 	}
+
 	@Test
 	public void deleTeest() throws Exception {
 		UserInfoDao userInfoDao = new UserInfoDaoImpl(sqlSessionFactory);
